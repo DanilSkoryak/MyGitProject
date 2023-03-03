e9418629d2bf0f7378bca937a122db3ff64fa680
Danil <Skoryak>
2023-03-03 20:30:48 +0100

    first-menu

--- Git.py
+++ Git.py
@@ -1,1 +1,10 @@
 print('Hello user')
+try:
+    num = int(input('Enter num: '))
+    while True:
+        try:
+            data_num = int(input('1-Change num, 2-zero num, 3-exit: '))
+        except ValueError:
+            print('Use only numbers')
+except ValueError:
+    print('Use only numbers')

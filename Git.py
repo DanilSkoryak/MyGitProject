f0741bdfd37935dc843e7e42867f1522c92d4b97
Danil <Skoryak>
2023-03-03 20:31:21 +0100

    next-menu

--- Git.py
+++ Git.py
@@ -4,6 +4,12 @@ try:
     while True:
         try:
             data_num = int(input('1-Change num, 2-zero num, 3-exit: '))
+            if data_num == 1:
+                num = int(input('Enter num: '))
+            elif data_num == 2:
+                num = 0
+            elif data_num == 3:
+                break
         except ValueError:
             print('Use only numbers')
 except ValueError:
